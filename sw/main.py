59da60711cf115615fadb9d4df52ff90bd13afa0
Beat Besmer <beat.besmer@gmail.com>
2023-12-22 16:06:19 +0100

    Make four rotations when a ball is detected

--- sw/main.py
+++ sw/main.py
@@ -8,16 +8,17 @@ from machine import Pin
 
 import cuboard
 from cuboard import readAndAdjust
-stepper = Stepper(HALF_STEP, Pin(14, Pin.OUT), Pin(12, Pin.OUT), Pin(13, Pin.OUT), Pin(15, Pin.OUT), delay=.003 )  
+stepper = Stepper(HALF_STEP, Pin(14, Pin.OUT), Pin(12, Pin.OUT), Pin(5, Pin.OUT), Pin(10, Pin.OUT), delay=.003 )  
 
-MARGIN = 0.8
+ROTATIONS = 4
+MARGIN = 0.9
 
-# photoresistor to 5V
+# photoresistor to 5V  
 # 10kOhm resistor to GND
 def sensorDetected(value):
-    global stepper, FULL_ROTATION
+    global stepper, FULL_ROTATION, ROTATIONS
     print("sensor detected with value " + str(value))
-    stepper.step(FULL_ROTATION, -1)
+    stepper.step(FULL_ROTATION * ROTATIONS, -1)
     
 
 print("Starting reading ...")
