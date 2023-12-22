b96ed8176117883e9d7aaa28954a22f6caec4a78
Beat Besmer <beat.besmer@gmail.com>
2023-12-22 20:33:53 +0100

    Add wifi capabilities

---
+++ sw/secrets.example.py
@@ -0,0 +1,9 @@
+class wifi:
+   aps = {
+       'SSID1': 'PASSWORD1',
+       'SSID2': 'PASSWORD2',       
+       }
+
+class mqtt:
+    host = 'mqtt://yourserver.com:1883'
+    topic = 'your.topic'
